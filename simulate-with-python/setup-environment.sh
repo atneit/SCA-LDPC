c713dbf92b909f83bb7e78c61dc65882575c3faf
Alexander Nilsson <a.code@nilsson.link>
2022-03-24 14:45:23 +0100

    Added python simulation environment.
    This is expected to be the source of the main simulation body.

---
+++ simulate-with-python/setup-environment.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+VENV_DEST="$SCRIPT_DIR/../python-virtualenv"
+VENV_DEST="$(realpath $VENV_DEST)"
+
+if [ ! -d "$VENV_DEST" ]; then
+    echo "Couldn't find virtual environment. Creating one..."
+    python3 -m venv $VENV_DEST
+fi
+
+echo "Activating python virtual envionment..."
+source $VENV_DEST/bin/activate
+
+echo "Making sure all specified python packages are installed..."
+pip install -r requirements.txt
+
+echo "Done!"
+echo
+echo -e "This script should be run as: "
+echo -e "\tsource $0"
+echo -e "if it wasn't you need to run the following to activate the virtual environment for the current shell:"
+echo -e "\tsource $VENV_DEST/bin/activate"

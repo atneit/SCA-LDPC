0ee626bd1aac313d74a53190ada81a4177d11df2
Alexander Nilsson <a.code@nilsson.link>
2022-03-28 16:11:40 +0200

    Command to simulate a random regular ldpc code

--- simulate-with-python/main.py
+++ simulate-with-python/main.py
@@ -13,13 +13,17 @@ from logzero import logger
 logger.debug("Importing dependencies...")
 
 # Import dependencies
-from simulate.make_code import make_qc_parity_check_matrix
+from simulate.make_code import (
+    make_qc_parity_check_matrix,
+    make_regular_ldpc_parity_check_matrix,
+)
 from simulate.utils import CommandsBase, pretty_string_matrix
 from simulate.decode import simulate_frame_error_rate
 from ldpc import bp_decoder
 from ldpc.codes import rep_code
 import numpy as np
 import argparse
+import sys
 
 
 logger.debug("Importing dependencies... Done")
@@ -33,14 +37,53 @@ class Commands(CommandsBase):
     PREFIX = "command_"
 
     def setup_arguments(self, parser: argparse.ArgumentParser):
-        pass
+        parser.add_argument(
+            "--seed",
+            action="store",
+            type=int,
+            required=False,
+            help="Use a fixed seed to make simulations reproducible.",
+        )
+        parser.add_argument(
+            "--runs",
+            action="store",
+            type=int,
+            default=100,
+            help="The number of runs to run the simulation for. Only relevant for simulations/commands that are non-deterministic.",
+        )
+        parser.add_argument(
+            "--error-rate",
+            action="store",
+            type=float,
+            default=0.05,
+            help="The error rate of the simulated binary symmetric channel.",
+        )
+
+    def command_regular_ldpc_code(self, args: argparse.Namespace):
+        logger.info(
+            "Testing a quasi cyclic ldpc code with a parity check matrix of the form: [H_0|H_1|I]"
+        )
+        runs = args.runs
+        error_rate = args.error_rate
+        k = 100  # 17669
+        rate = 0.5
+        r = int(k * rate)
+        row_weight = 6
+        column_weight = int(row_weight * rate)
+        # n = k + r
+        H = make_regular_ldpc_parity_check_matrix(k, r, column_weight, row_weight)
+        with np.printoptions(threshold=sys.maxsize):
+            logger.debug("Constructed parity check matrix:\n" + str(H))
+
+        successes = simulate_frame_error_rate(H, error_rate, runs)
+        logger.info(f"Success ratio {successes}/{runs}={successes/runs}")
 
     def command_qc_ldpc_code(self, args: argparse.Namespace):
         logger.info(
             "Testing a quasi cyclic ldpc code with a parity check matrix of the form: [H_0|H_1|I]"
         )
-        runs = 100
-        error_rate = 0.005
+        runs = args.runs
+        error_rate = args.error_rate
         k = 1000  # 17669
         num_blocks = 2
         r = int(k / num_blocks)
@@ -58,8 +101,8 @@ class Commands(CommandsBase):
         logger.info("official example")
 
         n = 13
-        error_rate = 0.3
-        runs = 5
+        error_rate = args.error_rate
+        runs = args.runs
         H = rep_code(n)
 
         successes = simulate_frame_error_rate(H, error_rate, runs)
--- simulate-with-python/simulate/make_code.py
+++ simulate-with-python/simulate/make_code.py
@@ -1,6 +1,7 @@
 from random import randint
 import numpy as np
 from scipy.linalg import circulant
+from . import utils
 
 
 def fixed_weight_vec(size, samplings: int):
@@ -35,3 +36,36 @@ def make_qc_parity_check_matrix(block_len: int, column_weight: int, num_blocks: 
 
     # Flatten into one big matrix
     return flatten_matrix_parts(parts)
+
+
+def make_regular_ldpc_parity_check_matrix(k, r, column_weight, row_weight, seed=None):
+    """
+    Constructs a regular ldpc parity check matrix.
+
+    The shape is [H_(r*k)|I_(r*r)] where k is the number of variable
+    nodes and r is the number of check nodes.
+    """
+    assert k * column_weight == r * row_weight
+
+    rng = utils.check_random_state(seed)
+
+    n = k + r
+
+    n_equations = (n * row_weight) // column_weight
+
+    block = np.zeros((n_equations // row_weight, n), dtype=int)
+    H = np.empty((n_equations, n))
+    block_size = n_equations // row_weight
+
+    # Filling the first block with consecutive ones in each row of the block
+
+    for i in range(block_size):
+        for j in range(i * column_weight, (i + 1) * column_weight):
+            block[i, j] = 1
+    H[:block_size] = block
+
+    # reate remaining blocks by permutations of the first block's columns:
+    for i in range(1, row_weight):
+        H[i * block_size : (i + 1) * block_size] = rng.permutation(block.T).T
+    H = H.astype(int)
+    return H

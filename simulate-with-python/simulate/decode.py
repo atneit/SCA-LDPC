f47ec3b9dc239ff14280cdd2ad53bab88b1b99b7
Denis Nabokov <zastiralkin@gmail.com>
2022-04-04 18:17:47 +0200

    Add errors provider class

--- simulate-with-python/simulate/decode.py
+++ simulate-with-python/simulate/decode.py
@@ -28,3 +28,58 @@ def simulate_frame_error_rate(
         successes += int(cmp)
 
     return successes
+
+class ErrorsProvider:
+    """
+    Class for generating errors according to specified distribution.
+
+    If file is not provided, each position is 1 with probability error_rate, otherwise 0.
+    File allows to manage other distributions. Each line corresponds to separate position,
+    where each line should contain n probabilities, divided by spaces and/or commas, that
+    add up to 1, where n is assumed to be odd. If n = 1, binary case is assumed, otherwise
+    generated errors lies in [-n/2, ..., n/2].
+    """
+
+    def __init__(self, error_rate, error_file, rnd):
+        self.error_rate = error_rate
+        self.error_distribution = None
+        self.rnd = rnd
+        import re
+        if error_file is not None:
+            error_distribution = []
+            with open(error_file, 'rt') as f:
+                for line if f:
+                    line = line.strip()
+                    pr = re.split('[, ]+', line)
+                    pr = list(float(x) for x in pr)
+                    error_distribution.append(pr)
+            self.error_distribution = error_distribution
+    
+    def __get_binary_error(self, threshold):
+        if self.rng.rand() < threshold:
+            return 1
+        else:
+            return 0
+
+    def get_error(self, pos):
+        """
+        Get error based on the distribution. Position pos is taken modulo length of file
+        (if it was provided, otherwise pos is ignored).
+        """
+        if self.error_distribution is None:
+            return __get_binary_error(self.error_rate)
+        l = len(self.error_distribution)
+        pr = self.error_distribution[pos % l]
+        pr_len = len(pr)
+        if pr_len == 1:
+            return __get_binary_error(pr[0])
+        else:
+            rand = self.rng.rand()
+            res = -(pr_len // 2)
+            threshold = 0
+            for p in pr:
+                threshold += p
+                if threshold > rand:
+                    return res
+                res += 1
+

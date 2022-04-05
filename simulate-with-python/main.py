be923d7ae3026dcd5e9f8ee434846c123204c1ca
Alexander Nilsson <a.code@nilsson.link>
2022-04-05 12:44:29 +0200

    Made a test command with 'doctest'

--- simulate-with-python/main.py
+++ simulate-with-python/main.py
@@ -8,11 +8,13 @@ __version__ = "0.1.0"
 __license__ = "MIT"
 
 # Activate logger
+import logging
 from logzero import logger
 
 logger.debug("Importing dependencies...")
 
 # Import dependencies
+import simulate
 from simulate.make_code import (
     make_qc_parity_check_matrix,
     make_regular_ldpc_parity_check_matrix,
@@ -20,7 +22,6 @@ from simulate.make_code import (
 )
 from simulate.utils import CommandsBase, make_random_state
 from simulate.decode import simulate_frame_error_rate
-from ldpc import bp_decoder
 from ldpc.codes import rep_code
 import numpy as np
 import argparse
@@ -148,6 +149,21 @@ class Commands(CommandsBase):
 
         successes = simulate_frame_error_rate(H, error_rate, runs, rng)
         logger.info(f"Success ratio {successes}/{runs}={successes/runs}")
+
+    def command_test(self, args: argparse.Namespace):
+        """This command runs all discovered doctests in the simulate package"""
+        import unittest
+        import doctest
+
+        suite = unittest.TestSuite()
+        suite.addTest(doctest.DocTestSuite(simulate.utils))
+        suite.addTest(doctest.DocTestSuite(simulate.make_code))
+        suite.addTest(doctest.DocTestSuite(simulate.decode))
+
+        logger.info("Disabling further logging output")
+        logging.disable(logging.CRITICAL)
+        runner = unittest.TextTestRunner(verbosity=2 if args.verbose else 0)
+        runner.run(suite)
 
 
 if __name__ == "__main__":

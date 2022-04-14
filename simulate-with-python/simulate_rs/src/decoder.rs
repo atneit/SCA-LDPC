bf3e7736e74d6eec2008d0e39cff3170c3d019cc
Alexander Nilsson <a.code@nilsson.link>
2022-04-14 16:26:33 +0200

    Into LLR

--- simulate-with-python/simulate_rs/src/decoder.rs
+++ simulate-with-python/simulate_rs/src/decoder.rs
@@ -12,12 +12,13 @@ use anyhow::Result;
 use g2p::GaloisField;
 use ndarray::Array1;
 use numpy::ndarray::{ArrayView1, ArrayView2};
+use ordered_float::NotNan;
 
 /// A variable node
 #[derive(Debug, Clone)]
 struct VariableNode<const DC: usize, const Q: usize> {
     /// options to deal with irregular codes
-    check_idx: [Option<Key1D>; DC],
+    check_idx: [Option<usize>; DC],
     /// The a-priory channel value
     channel: Option<Message<Q>>,
     /// The current decoded value
@@ -38,7 +39,7 @@ impl<const DC: usize, const Q: usize> Default for VariableNode<DC, Q> {
 #[derive(Debug)]
 struct CheckNode<const DV: usize> {
     /// options to deal with initialization and irregular codes
-    variable_idx: [Option<Key1D>; DV],
+    variable_idx: [Option<usize>; DV],
 }
 
 impl<const DV: usize> Default for CheckNode<DV> {
@@ -51,7 +52,7 @@ impl<const DV: usize> Default for CheckNode<DV> {
 
 type Llr = f64;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 struct Message<const Q: usize>([Llr; Q]);
 
 #[derive(Debug, Default, Clone)]
@@ -155,6 +156,12 @@ impl<
         GF: GaloisField,
     > Decoder<N, R, DV, DC, Q, GF>
 {
+    const N: usize = N;
+    const R: usize = R;
+    const DV: usize = DV;
+    const DC: usize = DC;
+    const Q: usize = Q;
+
     pub fn new(parity_check: [[GF; N]; R]) -> Self {
         let vn: RefCell<[VariableNode<DC, Q>; N]> = RefCell::new(make_default_array());
         let cn: RefCell<[CheckNode<DV>; R]> = RefCell::new(make_default_array());
@@ -178,15 +185,15 @@ impl<
                         edges.borrow_mut().insert(ij, Default::default());
 
                         // add the check index to the variable
-                        insert_first_none::<Key1D, DC>(
+                        insert_first_none::<_, DC>(
                             &mut vn.borrow_mut()[ij.col()].check_idx,
-                            ij.row,
+                            ij.row(),
                         );
 
                         // add the variable index to the check
-                        insert_first_none::<Key1D, DV>(
+                        insert_first_none::<_, DV>(
                             &mut cn.borrow_mut()[ij.row()].variable_idx,
-                            ij.col,
+                            ij.col(),
                         );
 
                         (ij, h)
@@ -212,13 +219,49 @@ impl<
         let mut vn = self.vn.clone();
         let mut edges = self.edges.clone();
 
-        // Initialize the channel values
+        // 1. Initialize the channel values
         vn.iter_mut().zip(channel_llr).for_each(move |(v, m)| {
             v.channel = Some(m);
             v.current = None;
         });
 
+        // 2. Check node update
+        for (check_idx, check) in self.cn.iter().enumerate() {
+            let sign = 1.0;
+            let min1 = f64::INFINITY;
+            let min2 = f64::INFINITY;
+            for var_idx in check.variable_idx.iter().flatten() {
+                let var = &vn[*var_idx];
+            }
+        }
+
         todo!()
+    }
+
+    pub fn into_llr(channel_output: &[[f64; Q]; N]) -> [Message<Q>; N] {
+        const EPSILON: f64 = 0.001;
+        let mut llrs = [Message::<Q>([0.0; Q]); N];
+        for (var, msg) in channel_output.iter().zip(llrs.iter_mut()) {
+            let sum: f64 = var.iter().sum();
+            let max = var
+                .iter()
+                .copied()
+                // ignore NAN values (errors from the previous line)
+                .flat_map(NotNan::new)
+                .max()
+                .map(NotNan::into_inner)
+                .expect("No maximum probability found");
+            // Ensure the probabilities sum to 1.0, taking into
+            // account the problem of floating point comparisons
+            assert!(sum < 1.0 + EPSILON);
+            assert!(sum > 1.0 - EPSILON);
+            // calculate LLR
+            for (ent, dst) in var.iter().zip(msg.0.iter_mut()) {
+                *dst = (max / ent).ln();
+            }
+        }
+
+        llrs
     }
 }
 
@@ -228,16 +271,58 @@ mod tests {
 
     g2p::g2p!(GF16, 4, modulus: 0b10011);
 
+    type MyTinyTestDecoder = Decoder<6, 3, 4, 3, { GF16::SIZE }, GF16>;
+
+    #[test]
+    fn into_llr() {
+        let channel_output = [[
+            0.0, 0.0, 0.0, 0.0, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.14, 0.02, 0.0, 0.0, 0.0, 0.0,
+        ]; MyTinyTestDecoder::N];
+        let llr = MyTinyTestDecoder::into_llr(&channel_output);
+        let expected = [Message([
+            f64::INFINITY,
+            f64::INFINITY,
+            f64::INFINITY,
+            f64::INFINITY,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            1.9459101490553135,
+            f64::INFINITY,
+            f64::INFINITY,
+            f64::INFINITY,
+            f64::INFINITY,
+        ]); MyTinyTestDecoder::N];
+        assert_eq!(expected, llr);
+    }
+
     #[test]
     fn it_works() {
-        let decoder_6_3_4_3_gf16 = Decoder::<6, 3, 4, 3, { GF16::SIZE }, GF16>::new([
+        let decoder_6_3_4_3_gf16 = MyTinyTestDecoder::new([
             [1.into(), 1.into(), 1.into(), 1.into(), 0.into(), 0.into()],
             [0.into(), 0.into(), 1.into(), 1.into(), 0.into(), 1.into()],
             [1.into(), 0.into(), 0.into(), 1.into(), 1.into(), 0.into()],
         ]);
 
+        // Zero message with zero noise
+        let mut channel_output = [[0.0; MyTinyTestDecoder::Q]; MyTinyTestDecoder::N];
+        for el in &mut channel_output {
+            el[0] = 1.0;
+        }
+
+        // Introduce an error
+        channel_output[1][0] = 0.1;
+        channel_output[1][15] = 0.9;
+
+        // Convert to LLR
+        let channel_llr = MyTinyTestDecoder::into_llr(&channel_output);
+
         let res = decoder_6_3_4_3_gf16
-            .product_sum([Message([0.0; GF16::SIZE]); 6])
+            .product_sum(channel_llr)
             .expect("Failed");
 
         let expected: [GF16; 6] = [0.into(); 6];

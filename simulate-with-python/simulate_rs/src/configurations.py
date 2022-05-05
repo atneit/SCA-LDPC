ec3669296bc6cf830323730b14c02d11ef31b8cf
Denis Nabokov <zastiralkin@gmail.com>
2022-05-05 14:33:30 +0200

    fix index

--- simulate-with-python/simulate_rs/src/configurations.py
+++ simulate-with-python/simulate_rs/src/configurations.py
@@ -53,5 +53,5 @@ while not finish:
         # sum(d_values) is equal to 0
         d_values[l-1] = -d
         send_d_values(connected_vars, d_values, beta, check_idx)
-    finish = increment_d_values(d_values, i)
+    finish = increment_d_values(d_values, l-1)
 print(beta)

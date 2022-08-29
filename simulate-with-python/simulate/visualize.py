bb2e59a16c51cf66ad2b3d083ca46c78de212c80
Alexander Nilsson <a.code@nilsson.link>
2022-08-29 14:59:03 +0200

    Rounding for 'oracle_calls' and 'unsatisfied'

--- simulate-with-python/simulate/visualize.py
+++ simulate-with-python/simulate/visualize.py
@@ -62,9 +62,13 @@ def view_hqc_simulation_csv(csv_file):
     if "checks" in df.columns:
         df = from_old_format(df)
 
+    # Round oracle_calls to nearest multiple of 200
+    df = round_stride_of_type(df, "oracle_calls", 500)
+    df = round_stride_of_type(df, "unsatisfied", 20)
+
     # Remove "oracle calls" and "unsatisfied", they are not interesting here
-    df = df[~df["stride_type"].isin(['oracle_calls', 'unsatisfied'])]
-    #df = df[df["weight"].isin([55])]
+    # df = df[~df["stride_type"].isin(['oracle_calls', 'unsatisfied'])]
+    df = df[df["weight"].isin([50])]
 
     logger.info("Plotting data")
     g = sns.relplot(
@@ -72,14 +76,21 @@ def view_hqc_simulation_csv(csv_file):
         x="stride",
         y="count",
         col="count_type",
-        row="weight",
-        hue="label",
+        row="stride_type",
+        #hue="label",
         kind="line",
         facet_kws={"margin_titles": True, "sharey": False, "sharex": False},
     )
-    g.set_axis_labels("Number of parity-checks", "Count")
-    g.set_titles(col_template="{col_name}")
-    g.tight_layout()
+    g.set_axis_labels("Number of ... (see right margin)", "Count of... (see top margin)")
+    g.set_titles(col_template="{col_name}", row_template="{row_name}")
+    g.add_legend()
+    #g.tight_layout()
 
     logger.info("Writing data to file")
-    plt.savefig("view_hqc_simulation_csv.png")
+    plt.savefig("view_hqc_simulation_csv.pdf")
+
+def round_stride_of_type(df, stride_type, multiple_of):
+    cond = df["stride_type"] == stride_type
+    half = multiple_of//2
+    df.loc[cond,"stride"] = df.loc[cond,"stride"].apply(lambda s: (s+half) / multiple_of).astype(int) * 100
+    return df

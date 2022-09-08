a523d3ba9097518321c5026c36fba6bc533caaf3
Alexander Nilsson <a.code@nilsson.link>
2022-09-08 16:49:36 +0200

    More work on visualizations

--- simulate-with-python/simulate/visualize.py
+++ simulate-with-python/simulate/visualize.py
@@ -9,6 +9,7 @@ rootlogger.setLevel(level="INFO")
 logger = logging.getLogger(__name__)
 
 import sys
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -19,8 +20,8 @@ sns.set_theme(style="ticks")
 rootlogger.setLevel(origlevel)
 
 
-def from_old_format(df: pd.DataFrame):
-    logger.info(f"Converting this dataframe to new format: \n{df}")
+def wide_to_long_format(df: pd.DataFrame):
+    logger.info(f"Converting this DataFrame from wide to long: \n{df}")
     meta_col = ["label", "alg", "weight", "epsilon0", "epsilon1"]
     new_df = pd.DataFrame(
         columns=meta_col + ["stride_type", "stride", "count_type", "count"]
@@ -31,6 +32,8 @@ def from_old_format(df: pd.DataFrame):
             "bad_flips",
             "found_bad_satisfied_checks",
             "found_bad_unsatisfied_checks",
+            "remaining-flips",
+            "found_bad_checks",
         ]:
             new_df = extract_pair(df, meta_col, new_df, old_stride, old_count)
 
@@ -59,16 +62,56 @@ def view_hqc_simulation_csv(csv_file):
     logger.info(f"Reading file: {csv_file}")
     df = pd.read_csv(csv_file)
 
-    if "checks" in df.columns:
-        df = from_old_format(df)
+    # Combine bit flips to calculate number of correct bit positions, as a percentage
+    max_flips = df["good_flips"].max()
+    df["remaining-flips"] = max_flips + df["bad_flips"] - df["good_flips"]
+    df["found_bad_checks"] = (
+        df["found_bad_unsatisfied_checks"] + df["found_bad_satisfied_checks"]
+    )
+
+    df = wide_to_long_format(df)
 
     # Round oracle_calls to nearest multiple of 200
     df = round_stride_of_type(df, "oracle_calls", 500)
     df = round_stride_of_type(df, "unsatisfied", 20)
 
     # Remove "oracle calls" and "unsatisfied", they are not interesting here
-    # df = df[~df["stride_type"].isin(['oracle_calls', 'unsatisfied'])]
-    df = df[df["weight"].isin([50])]
+    # Filter away uninteresting data
+    df = df[
+        (
+            (True)
+            & (df["weight"].isin([50]))
+            & (df["stride_type"].isin(["checks", "oracle_calls"]))
+            & (df["count_type"].isin(["remaining-flips", "found_bad_checks"]))
+        )
+    ]
+
+    # Rename for human readable axis labels
+    df["stride_type"] = df["stride_type"].cat.rename_categories(
+        {
+            "checks": "parity checks",
+            "oracle_calls": "oracle calls",
+            "unsatisfied": "unsatisfied parity checks",
+        }
+    )
+    df["count_type"] = df["count_type"].cat.rename_categories(
+        {
+            "good_flips": "correct bit-flips",
+            "bad_flips": "incorrect bit-flips",
+            "found_bad_unsatisfied_checks": "found_bad_unsatisfied_checks",
+            "found_bad_satisfied_checks": "found_bad_satisfied_checks",
+            "remaining-flips": "remaining bit-flips",
+            "found_bad_checks": "detected bad parity checks",
+        }
+    )
+
+    df["stride_type"] = df["stride_type"].cat.remove_unused_categories()
+    df["count_type"] = df["count_type"].cat.remove_unused_categories()
+
+    logger.info(
+        "stride_types remaining: " + str(list(df["stride_type"].cat.categories))
+    )
+    logger.info("count_types remaining: " + str(list(df["count_type"].cat.categories)))
 
     logger.info("Plotting data")
     g = sns.relplot(
@@ -77,20 +120,68 @@ def view_hqc_simulation_csv(csv_file):
         y="count",
         col="count_type",
         row="stride_type",
-        #hue="label",
+        hue="label",
         kind="line",
         facet_kws={"margin_titles": True, "sharey": False, "sharex": False},
     )
-    g.set_axis_labels("Number of ... (see right margin)", "Count of... (see top margin)")
+    for ((row, col), ax) in g.axes_dict.items():
+        success_stride = np.array([])
+        ax2 = ax.twinx()
+        ax2.tick_params(axis="y", right=False)
+        if col in ["remaining bit-flips"]:
+            mask = (
+                (df["count_type"] == col)
+                & (df["stride_type"] == row)
+                & (df["success"] == True)
+            )
+            success_stride = df.loc[mask, "stride"].to_numpy()
+        box = sns.boxplot(
+            x=success_stride,
+            ax=ax2,
+            orient="h",
+            width=0.05,
+            notch=False,
+            whis=(0, 100),
+            color="white",
+            saturation=0,
+            boxprops={'facecolor':'none', 'edgecolor':'black'},
+            medianprops={'color':'black'},
+            whiskerprops={'color':'black'},
+            capprops={'color':'black'}
+        )
+        if success_stride.size > 0:
+            logger.info(f"Added boxplot to ({row},{col})")
+            lines = box.get_lines()
+            boxes = [c for c in box.get_children() if type(c).__name__ == "PathPatch"]
+            lines_per_box = int(len(lines) / len(boxes))
+            for median in lines[4 : len(lines) : lines_per_box]:
+                x, y = (data.mean() for data in median.get_data())
+                box.annotate(
+                    "Decoding success",
+                    xy=(x, y),
+                    xycoords="data",
+                    xytext=(30, 30),
+                    textcoords="offset pixels",
+                    horizontalalignment='center',
+                    verticalalignment='top',
+                    clip_on=True,
+                )
+    g.set_axis_labels(
+        "Number of ... (see right margin)", "Number of ... (see top margin)"
+    )
     g.set_titles(col_template="{col_name}", row_template="{row_name}")
-    g.add_legend()
-    #g.tight_layout()
+    # g.add_legend()
+    # g.tight_layout()
 
     logger.info("Writing data to file")
-    plt.savefig("view_hqc_simulation_csv.pdf")
+    g.savefig("view_hqc_simulation_csv.pdf")
+
 
 def round_stride_of_type(df, stride_type, multiple_of):
     cond = df["stride_type"] == stride_type
-    half = multiple_of//2
-    df.loc[cond,"stride"] = df.loc[cond,"stride"].apply(lambda s: (s+half) / multiple_of).astype(int) * 100
+    half = multiple_of // 2
+    df.loc[cond, "stride"] = (
+        df.loc[cond, "stride"].apply(lambda s: (s + half) / multiple_of).astype(int)
+        * multiple_of
+    )
     return df

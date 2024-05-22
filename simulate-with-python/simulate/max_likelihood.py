2737867931a1635d91d636c993d2096fc9e5b735
Denis Nabokov <denis.nabokov@eit.lth.se>
2024-05-22 17:06:55 +0200

    functions to compute max likelihood decoding

---
+++ simulate-with-python/simulate/max_likelihood.py
@@ -0,0 +1,148 @@
+import itertools as it
+
+
+def pr_cond_yx(y, x, p):
+    # compute Pr[Y = y | X = x]
+    res = 1
+    for i in range(len(x)):
+        if x[i] == y[i]:
+            res *= p
+        else:
+            res *= 1 - p
+    return res
+
+
+def pr_y(y, p, secret_range_func, coding, distrib_secret, sum_weight):
+    # compute Pr[Y = y]
+    res = 0
+    for s in secret_range_func(sum_weight):
+        xprime = coding[s]
+        pr_xprime_y = distrib_secret[s] * pr_cond_yx(y, xprime, p)
+        res += pr_xprime_y
+    return res
+
+
+def pr_cond_xy(
+    s, y, p, secret_range_func, coding, distrib_secret, sum_weight, pr_y_saved=None
+):
+    # compute Pr[X = coding[s] | Y = y]
+    if pr_y_saved is None:
+        pr_y_saved = pr_y(y, p, secret_range_func, coding, distrib_secret, sum_weight)
+    return pr_cond_yx(y, coding[s], p) * distrib_secret[s] / pr_y_saved
+
+
+def pr_of_y_from_prediction(pred_y, y):
+    res = 1
+    for p, yval in zip(pred_y, y):
+        if yval == 0:
+            res *= 1 - p
+        else:
+            res *= p
+    return res
+
+
+def s_distribution_from_hard_y(
+    y, p, secret_range_func, coding, distrib_secret, sum_weight
+):
+    distr = [0] * len(coding)
+    for i, s in enumerate(secret_range_func(sum_weight)):
+        distr[i] = pr_cond_xy(
+            s,
+            y,
+            p,
+            secret_range_func,
+            coding,
+            distrib_secret,
+            sum_weight,
+            None,
+        )
+    return distr
+
+
+def pr_cond_yx_adaptive(y, s, p, coding_tree):
+    # compute Pr[Y = y | X = e(s)]
+    res = 1
+    node = coding_tree
+    for y_val in y:
+        if s < node.value:
+            # y_val should be 1
+            if y_val == 1:
+                res *= p
+                node = node.left
+            else:
+                res *= 1 - p
+                node = node.right
+        else:
+            # y_val should be 0
+            if y_val == 1:
+                res *= 1 - p
+                node = node.left
+            else:
+                res *= p
+                node = node.right
+    return res
+
+
+def pr_y_adaptive(y, p, secret_range_func, coding_tree, distrib_secret, sum_weight):
+    # compute Pr[Y = y]
+    res = 0
+    for s in secret_range_func(sum_weight):
+        pr_xprime_y = distrib_secret[s] * pr_cond_yx_adaptive(y, s, p, coding_tree)
+        res += pr_xprime_y
+    return res
+
+
+def pr_cond_xy_adaptive(
+    s,
+    y,
+    p,
+    secret_range_func,
+    coding_tree,
+    distrib_secret,
+    sum_weight,
+    pr_y_saved=None,
+):
+    # compute Pr[X = e(s) | Y = y]
+    if pr_y_saved is None:
+        pr_y_saved = pr_y_adaptive(
+            y, p, secret_range_func, coding_tree, distrib_secret, sum_weight
+        )
+    return pr_cond_yx_adaptive(y, s, p, coding_tree) * distrib_secret[s] / pr_y_saved
+
+
+def s_distribution_from_hard_y_adaptive(
+    y, p, secret_range_func, coding_tree, distrib_secret, sum_weight
+):
+    distr = [0] * len(distrib_secret)
+    for i, s in enumerate(secret_range_func(sum_weight)):
+        distr[i] = pr_cond_xy_adaptive(
+            s,
+            y,
+            p,
+            secret_range_func,
+            coding_tree,
+            distrib_secret,
+            sum_weight,
+            None,
+        )
+    return distr
+
+
+def s_distribution_from_prediction_y(
+    pred_y, p, secret_range_func, coding, distrib_secret, sum_weight
+):
+    distr = [0] * len(secret_range_func(sum_weight))
+    for y in it.product(range(2), repeat=len(coding[0])):
+        pr_y_saved = pr_y(y, p, secret_range_func, coding, distrib_secret, sum_weight)
+        for i, s in enumerate(secret_range_func(sum_weight)):
+            distr[i] += pr_cond_xy(
+                s,
+                y,
+                p,
+                secret_range_func,
+                coding,
+                distrib_secret,
+                sum_weight,
+                pr_y_saved,
+            ) * pr_of_y_from_prediction(pred_y, y)
+    return distr
